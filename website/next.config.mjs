a231256b988f33d6ff41aaeaad2279fd5776a071
not VFLC <radioactive-lollipop@proton.me>
2023-07-31 13:01:22 +0800

    add github to login options

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -19,7 +19,7 @@ const config = {
     defaultLocale: "en",
   },
   images: {
-    domains: ["cdn.discordapp.com"],
+    domains: ["avatars.githubusercontent.com", "cdn.discordapp.com"],
   },
 };
 
